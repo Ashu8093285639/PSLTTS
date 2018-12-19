2dd9cdf452cd2518032d66b47f92520add084850
Dese Narfa Firmansyah <desenfirman@GMAIL.COM>
2018-12-19 16:43:51 +0700

    do some code refactoring

--- backpropagation_neural_net.py
+++ backpropagation_neural_net.py
@@ -1,6 +1,10 @@
-import pandas as pd
-import numpy as np
-import random as rd
+from numpy import abs
+from numpy import exp
+from random import random
+
+
+def un_normalized(value, min, max):
+    return value * (max - min) + min
 
 
 class Neuron:
@@ -11,163 +15,150 @@ class Neuron:
         self.delta = list()
 
 
-INPUT_LAYER = 5
-HIDDEN_LAYER = 3
-OUTPUT_LAYER = 1
-LEARNING_RATE = 0.5
+class BackpropagationNN:
 
-# total bobot = 22
-# 0,1,2,3,4,5
+    def __init__(self, input_layer, hidden_layer, output_layer, learning_rate):
+        self.INPUT_LAYER = input_layer
+        self.HIDDEN_LAYER = hidden_layer
+        self.OUTPUT_LAYER = output_layer
+        self.LEARNING_RATE = learning_rate
 
+    def summingFunction(self, last_weight, training_data):
+        bias = last_weight[-1]  # bias on last index
+        y_in = 0
+        for idx in range(0, len(last_weight) - 1):
+            y_in += last_weight[idx] * training_data[idx]
+        return bias + y_in
 
-def summingFunction(last_weight, training_data):
-    bias = last_weight[-1]  # bias on last index
-    y_in = 0
-    for idx in range(0, len(last_weight) - 1):
-        y_in += last_weight[idx] * training_data[idx]
-    return bias + y_in
+    def activation(self, y_in):
+        return 1.0 / (1.0 + exp(-1 * y_in))
 
+    def derivativeFunction(self, y):
+        return y * (1.0 - y)
 
-def activation(y_in):
-    return 1.0 / (1.0 + np.exp(-1 * y_in))
+    def initWeight(self):
+        layer = list()
+        input_to_hidden = [
+            Neuron([random() for i in range(
+                self.INPUT_LAYER + 1)]) for j in range(self.HIDDEN_LAYER)]
+        layer.append(input_to_hidden)
+        hidden_to_output = [
+            Neuron([0 for i in range(
+                self.HIDDEN_LAYER + 1)]) for j in range(self.OUTPUT_LAYER)]
+        layer.append(hidden_to_output)
+        self.net = layer
 
+    def feedForward(self, row):
+        processed_layer = row
+        for layer in self.net:
+            # print(layer)
+            new_inputs = list()
+            for neuron in layer:
 
-def derivativeFunction(y):
-    return y * (1.0 - y)
+                # print(neuron)
+                y_in = self.summingFunction(neuron.weight, processed_layer)
+                neuron.output = self.activation(y_in)
+                new_inputs.append(neuron.output)
+            processed_layer = new_inputs
+        return processed_layer  # nilai y1, y2, .... yx
 
+    def backPpError(self, target):
+        for id_layer in reversed(range(len(self.net))):
+            layer = self.net[id_layer]
+            # print(id_layer)
+            error_factor = list()
+            if id_layer == len(self.net) - 1:  # error_factor dari output
+                for weight_id in range(len(layer)):
+                    neuron = layer[weight_id]
+                    error_factor.append(target[weight_id] - neuron.output)
+            else:
+                for weight_id in range(len(layer)):
+                    error_factor_sum = 0.0
+                    prev_layer = self.net[id_layer - 1]
+                    for neuron in prev_layer:
+                        error_factor_sum += neuron.weight[weight_id] * neuron.delta
+                    error_factor.append(error_factor_sum)
 
-def initWeight():
-    layer = list()
-    input_to_hidden = [
-        Neuron([rd.random() for i in range(INPUT_LAYER + 1)]) for j in range
-        (HIDDEN_LAYER)]
-    layer.append(input_to_hidden)
-    hidden_to_output = [
-        Neuron([rd.random() for i in range(HIDDEN_LAYER + 1)]) for j in range
-        (OUTPUT_LAYER)]
-    layer.append(hidden_to_output)
-    return layer
-
-
-def initWeightPSO(partikel):
-    layer = list()
-    partikel_dimens_idx = 0
-    input_to_hidden = list()
-
-    for i in range(HIDDEN_LAYER):
-        w = list()
-        for j in range(INPUT_LAYER + 1):
-            w.append(partikel[partikel_dimens_idx])
-            partikel_dimens_idx += 1
-        input_to_hidden.append(Neuron(w))
-    layer.append(input_to_hidden)
-
-    hidden_to_output = list()
-    for i in range(OUTPUT_LAYER):
-        w = list()
-        for j in range(HIDDEN_LAYER + 1):
-            w.append(partikel[partikel_dimens_idx])
-            partikel_dimens_idx += 1
-        hidden_to_output.append(Neuron(w))
-    layer.append(hidden_to_output)
-    return layer
-
-
-def feedForward(net, row):
-    processed_layer = row
-    for layer in net:
-        # print(layer)
-        new_inputs = list()
-        for neuron in layer:
-
-            # print(neuron)
-            y_in = summingFunction(neuron.weight, processed_layer)
-            neuron.output = activation(y_in)
-            new_inputs.append(neuron.output)
-        processed_layer = new_inputs
-    return processed_layer  # nilai y1, y2, .... yx
-
-
-def backPpError(target):
-    for id_layer in reversed(range(len(net))):
-        layer = net[id_layer]
-        # print(id_layer)
-        error_factor = list()
-        if id_layer == len(net) - 1:  # error_factor dari output
             for weight_id in range(len(layer)):
                 neuron = layer[weight_id]
-                error_factor.append(target[weight_id] - neuron.output)
-        else:
-            for weight_id in range(len(layer)):
-                error_factor_sum = 0.0
-                prev_layer = net[id_layer - 1]
-                for neuron in prev_layer:
-                    error_factor_sum += neuron.weight[weight_id] * neuron.delta
-                error_factor.append(error_factor_sum)
+                neuron.delta = error_factor[weight_id] * self.derivativeFunction(neuron.output)
 
-        for weight_id in range(len(layer)):
-            neuron = layer[weight_id]
-            neuron.delta = error_factor[weight_id] * derivativeFunction(
-                neuron.output)
+    def updateWeight(self, row):
+        for i in range(len(self.net)):
+            inputs = list()
+            if i == 0:
+                inputs = row
+            else:
+                prev_net = self.net[i - 1]
+                inputs = [neuron.output for neuron in prev_net]
+            # print(inputs)
+            for neuron in self.net[i]:
+                neuron.weight[-1] += self.LEARNING_RATE * neuron.delta
+                for weight_id in range(0, len(inputs)):
+                    # print(weight_id)
+                    neuron.weight[weight_id] += self.LEARNING_RATE * (
+                        neuron.delta - inputs[weight_id])
 
+    def training(self, X_train, Y_train, max_value, min_value, total_epoch):
+        # print(self.net[0][0].weight)
+        X_list = X_train.values.tolist()
+        Y_list = [[x] for x in Y_train.values.tolist()]
+        fitness = 0
+        for epoch in range(total_epoch):
+            mape = 0
+            for idx, data_row in enumerate(Y_list):
+                self.feedForward(X_list[idx])
+                self.backPpError(Y_list[idx])
+                self.updateWeight(X_list[idx])
+            for x in range(len(Y_list)):
+                actual_value = un_normalized(
+                    Y_list[x][0], max_value, min_value)
+                forecasted_value = un_normalized(
+                    self.predict(X_list[x])[0], max_value, min_value)
+                # print('Test: ', actual_value, " ", forecasted_value)
+                mape += abs(
+                    (actual_value - forecasted_value) / actual_value)
+                # print("Mape sum: ", mape)
+            mape = mape * 100 / len(Y_train)
+            fitness = 100 / (100 + mape)
+            print('epoch %s, MAPE %s, FITNESS %s' % (epoch, mape, fitness))
+            # printNetworkWeight()
+        return fitness
 
-def updateWeight(row):
-    for i in range(len(net)):
-        inputs = list()
-        if i == 0:
-            inputs = row
-        else:
-            prev_net = net[i - 1]
-            inputs = [neuron.output for neuron in prev_net]
-        # print(inputs)
-        for neuron in net[i]:
-            neuron.weight[-1] += LEARNING_RATE * neuron.delta
-            for weight_id in range(0, len(inputs)):
-                # print(weight_id)
-                neuron.weight[weight_id] += LEARNING_RATE * (
-                    neuron.delta - inputs[weight_id])
+    def predict(self, row):
+        output = self.feedForward(row)
+        return output
 
-
-def training(net, X_train, Y_train, total_epoch):
-    X_list = X_train.values.tolist()
-    Y_list = [[x] for x in Y_train.values.tolist()]
-    fitness = 0
-    for epoch in range(total_epoch):
+    def data_testing(self, X_test, Y_test, max_value, min_value):
+        X_test = X_test.values.tolist()
+        fitness = 0
         mape = 0
-        actual_value = list()
-        for idx, data_row in enumerate(X_list):
-            feedForward(net, X_list[idx])
-            actual_value.append(un_normalized(Y_train[idx]))
-            backPpError(Y_list[idx])
-            updateWeight(X_list[idx])
-        for x in range(len(actual_value)):
-            forecasted_value = un_normalized(predict(net, X_list[x])[0])
-            # print('Test: ', actual_value[x], " ", forecasted_value)
-            mape += np.abs(
-                (actual_value[x] - forecasted_value) / actual_value[x])
-        mape = mape * 100 / len(actual_value)
-        fitness = 100 / (100 + mape)
-        print('epoch %s, MAPE %s, FITNESS %s' % (epoch, mape, fitness))
+        for x, data_row in enumerate(Y_test):
+            actual_value = un_normalized(Y_test.iloc[x], max_value, min_value)
+            forecasted_value = un_normalized(
+                self.predict(X_test[x])[0], max_value, min_value)
+            # print('Test: ', actual_value, " ", forecasted_value)
+            mape += abs((actual_value - forecasted_value) / actual_value)
+            # print("Mape sum: ", mape)
+            mape = mape * 100 / len(Y_train)
+            fitness = 100 / (100 + mape)
+        print('MAPE %s, FITNESS %s' % (mape, fitness))
         # printNetworkWeight()
-        #print()
-    return fitness
+        return fitness
 
-
-def predict(net, row):
-    output = feedForward(net, row)
-    return output
-
-
-def un_normalized(value):
-    return value * (max(dataset_value) - 
-        min(dataset_value)) + min(dataset_value)
 
 
 df = pd.DataFrame([list(i) for i in zip(*time_series_dataset)])
 
-X_train = df.iloc[:, :5]
-Y_train = df.iloc[:, 5]
+X_train = df.iloc[0:90, :4]
+Y_train = df.iloc[0:90:, 4]
+X_test = df.iloc[71:80, :4]
+Y_test = df.iloc[71:80, 4]
 
-net = initWeight()
-training(net, X_train, Y_train, 100)
+max_dataset = 1292.0
+min_dataset = 538.0
 
+backpp = BackpropagationNN(4, 3, 1, 0.4)
+backpp.initWeight()
+backpp.training(X_train, Y_train, max_dataset, min_dataset, 100)
+backpp.data_testing(X_test, Y_test, max_dataset, min_dataset)
--- particle_swarm_optimization.py
+++ particle_swarm_optimization.py
@@ -1,5 +1,4 @@
-import random as rd
-
+from random import random
 
 XMIN = 0
 XMAX = 1
@@ -32,89 +31,96 @@ def position_clamping(xnew, xmin, xmax):
 VMIN, VMAX = velocity_boundary(0.6, XMIN, XMAX)
 
 
-def get_fitness(partikel):
-    net = list()
-    net = initWeightPSO(partikel)
-    fitness = training(net, X_train, Y_train, 10)
-    return fitness
+class Particle():
+
+    XMAX = 1
+    XMIN = 0
+
+    def __init__(self, particle_size):
+
+        self.position = [(XMIN + random() * (
+            XMAX - XMIN)) for i in range(particle_size)]
+        self.velocity = [0 for i in range(particle_size)]
+        self.fitness = self.get_fitness(self.position)
+
+    def get_fitness(particle_position):
+        # net = list()
+        # net = initWeightPSO(partikel)
+        # fitness = training(net, X_train, Y_train, 10)
+        fitness = 0
+        for val in particle_position:
+            fitness += val
+        return fitness
 
 
-def initPopulasi(pop_size, partikel_size):
-    pop = [{
-        "position":
-        [(XMIN + rd.random() * (XMAX - XMIN)) for i in range(partikel_size)],
-        "velocity":
-        [0 for i in range(partikel_size)]} for n in range(pop_size)]
-    for indv_idx in range(len(pop)):
-        pop[indv_idx]["fitness"] = get_fitness(pop[indv_idx]["position"])
-    return pop
+class ParticleSwarmOptimization():
+
+    def __init__(self, pop_size, particle_size):
+        self.initPops(pop_size, particle_size)
+
+    def initPops(self, pop_size, particle_size):
+        self.pops = [Particle(particle_size) for n in range(pop_size)]
+        self.p_best = self.pops
+        self.g_best = self.get_g_best()
+
+    def get_g_best(self):
+        p_best_sorted = self.p_best
+        p_best_sorted = sorted(
+            p_best_sorted, key=lambda partc: partc.fitness, reverse=True)
+        return p_best_sorted[0]
+
+    def update_velocity_and_position(self, w, c1, c2):
+        updated_pops = self.pops
+        for partc_id, particle in enumerate(self.pops):
+            for partc_id_dimen in range(len(particle.velocity)):
+                #  update velocity
+                vnew = (
+                    w * particle.velocity[partc_id_dimen] +
+                    c1 * random() *
+                    (self.p_best[partc_id].position[partc_id_dimen] -
+                        particle.position[partc_id_dimen]) +
+                    c2 * random() +
+                    (self.g_best.position[partc_id_dimen] -
+                        particle.position[partc_id_dimen])
+                )
+
+                # update velocity
+                vnew = velocity_clamping(vnew, VMIN, VMAX)
+                updated_pops[partc_id].velocity[partc_id_dimen] = vnew
+
+                # update position
+                xnew = particle.position[partc_id_dimen] + vnew
+                updated_pops[partc_id].position[partc_id_dimen] = xnew
+
+            new_particle_fitness = self.get_fitness(
+                updated_pops[partc_id].position)
+            updated_pops[partc_id].fitness = new_particle_fitness
+
+        self.pops = updated_pops
+
+    def update_p_best(self):
+        for partc_id in range(len(self.pops)):
+            if(self.pops[partc_id].fitness > self.p_best[partc_id].fitness):
+                self.p_best[partc_id] = self.pops[partc_id]
+        # print(p_best)
+
+    def optimize(self, tmax):
+        t = 0
+        while(t < tmax):
+            w = 0.4 + (0.9 - 0.4) * ((tmax - t) / tmax)
+            c1 = (0.5 - 2.5) * (t / tmax) + 2.5
+            c2 = (0.5 - 2.5) * (t / tmax) + 0.5
+            self.update_velocity_and_position(w, c1, c2)
+            self.update_p_best()
+            self.g_best = self.get_g_best()
+            # print(p_best_pops)
+            print(self.g_best.position)
+            print(self.g_best.fitness)
+            t += 1
 
 
-def update_velocity_and_position(pops, p_best_pops, g_best, w, c1, c2):
-    new_pops = list()
-
-    for id_partikel, partikel in enumerate(pops):
-        new_particle_velocity = list()
-        new_particle_position = list()
-        for id_dimensi in range(len(partikel['velocity'])):
-            #  update velocity
-            vnew = (
-                w * partikel['velocity'][id_dimensi] +
-                c1 * rd.random() *
-                (p_best_pops[id_partikel]['position'][id_dimensi] -
-                    partikel['position'][id_dimensi]) +
-                c2 * rd.random() +
-                (g_best['position'][id_dimensi] -
-                    partikel['position'][id_dimensi])
-            )
-            vnew = velocity_clamping(vnew, VMIN, VMAX)
-            # print(vnew)
-            new_particle_velocity.append(vnew)
-
-            #  update position
-            xnew = partikel['position'][id_dimensi] + vnew
-            new_particle_position.append(xnew)
-
-        new_particle_fitness = get_fitness(new_particle_position)
-        new_pops.append({
-            "position": new_particle_position,
-            "velocity": new_particle_velocity,
-            "fitness": new_particle_fitness
-        })
-
-    return new_pops
-
-
-def update_p_best(pops, new_pops):
-    p_best = pops
-    for idx in range(len(pops)):
-        if(new_pops[idx]['fitness'] > pops[idx]['fitness']):
-            p_best[idx] = new_pops[idx]
-    # print(p_best)
-    return p_best
-
-
-def update_g_best(pbest_pops):
-    sorted_pbest = sorted(pbest_pops, key=lambda k: k['fitness'], reverse=True)
-    return sorted_pbest[0]
-
-
-pops = initPopulasi(1, 22)
-p_best_pops = pops
-g_best = update_g_best(p_best_pops)
-
-t = 0
-tmax = 10
-while(t < tmax):
-    w = 0.4 + (0.9 - 0.4) * ((tmax - t) / tmax)
-    c1 = (0.5 - 2.5) * (t / tmax) + 2.5
-    c2 = (0.5 - 2.5) * (t / tmax) + 0.5
-    new_pops = update_velocity_and_position(
-        pops, p_best_pops, g_best, w, c1, c2)
-    p_best_pops = update_p_best(p_best_pops, new_pops)
-    # print(p_best_pops)
-    pops = p_best_pops
-    g_best = update_g_best(pops)
-    # print(g_best)
-    print(pops)
-    t += 1
+pso = ParticleSwarmOptimization(10, 10)
+pso.optimize(10)
+# pops = initPopulasi(1, 22)
+# p_best_pops = pops
+# g_best = update_g_best(p_best_pops)
